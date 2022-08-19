2e27924a36a2245bd7a6365e34703cfd6eb57510
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2022-08-20 03:16:44 +0800

    deleting current post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: %i[show edit update destroy]
   def new
     @post = Post.new
   end
@@ -13,17 +14,11 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update(post_params)
       redirect_to post_path(@post)
     else
@@ -31,7 +26,17 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy
+
+    redirect_to root_path
+  end
+
   private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :body)
